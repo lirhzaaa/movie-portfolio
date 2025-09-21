d372021cf7d61616c8c2bafcda41c7c878dc8cb7
lirhzaaa <mhmdazhrilnmldn@gmail.com>
2025-09-21 16:42:56 +0700

    fat: update header

--- src/components/Elements/Header/HeaderItems.jsx
+++ src/components/Elements/Header/HeaderItems.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const HeaderItems = (props) => {
   const { name, Icon } = props;
   return (
-    <div className="text-white flex items-center cursor-pointer gap-3 text-[18px] font-semibold hover:underline underline-offset-8 mb-2">
+    <div className="text-white flex items-center cursor-pointer gap-3 text-[18px] font-semibold mb-2 hover:underline underline-offset-8 transition-all duration-300">
         <Icon className="h-4" />
         <h2 className="">{name}</h2>
     </div>
